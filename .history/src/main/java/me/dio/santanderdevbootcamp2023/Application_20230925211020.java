738e159e92d321a3ffb23aad6a5d72f3f459e0cb
Eziocdl <eziocdl@gmail.com>
2023-09-26 11:48:25 -0300

    Initial commit

---
+++ .history/src/main/java/me/dio/santanderdevbootcamp2023/Application_20230925211020.java
@@ -0,0 +1,13 @@
+package me.dio.santanderdevbootcamp2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderDevBootcamp2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevBootcamp2023Application.class, args);
+	}
+
+}
---
+++ src/main/java/me/dio/santanderdevbootcamp2023/Application.java
@@ -0,0 +1,14 @@
+package me.dio.santanderdevbootcamp2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderDevBootcamp2023Application.class, args);
+	}
+
+}
+ 
